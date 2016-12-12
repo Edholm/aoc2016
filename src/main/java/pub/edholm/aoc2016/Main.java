c918af5960123932d8950dde50e97f71b2ed19d8
Emil Edholm <bobby@verypublic.org>
2016-12-12 21:37:15 +0100

    Day 7 stub

--- src/main/java/pub/edholm/aoc2016/Main.java
+++ src/main/java/pub/edholm/aoc2016/Main.java
@@ -5,6 +5,8 @@ import pub.edholm.aoc2016.day2.Day2;
 import pub.edholm.aoc2016.day3.Day3;
 import pub.edholm.aoc2016.day4.Day4;
 import pub.edholm.aoc2016.day5.Day5;
+import pub.edholm.aoc2016.day6.Day6;
+import pub.edholm.aoc2016.day7.Day7;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -21,7 +23,9 @@ public class Main {
                 Day2.class,
                 Day3.class,
                 Day4.class,
-                Day5.class);
+                Day5.class,
+                Day6.class,
+                Day7.class);
 
         for (Class<? extends Aoc> day : days) {
             Method mainMethod = day.getMethod("main", String[].class);
---
+++ src/main/java/pub/edholm/aoc2016/day7/Day7.java
@@ -0,0 +1,16 @@
+package pub.edholm.aoc2016.day7;
+
+import pub.edholm.aoc2016.Aoc;
+
+/**
+ * Created by Emil Edholm on 2016-12-12.
+ */
+public class Day7 implements Aoc {
+    public static void main(String[] args) {
+        final Day7 day7 = new Day7();
+
+        System.out.println("Day 7:");
+        System.out.println("\tPart 1: " + day7.solvePart1("day7.input"));
+        System.out.println("\tPart 2: " + day7.solvePart2("day7.input"));
+    }
+}
